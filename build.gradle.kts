3fde54fe448dc4040641ddedb58ce12bf8c0c2f1
juliuskrah <9932477+juliuskrah@users.noreply.github.com>
2023-09-15 13:27:30 +0200

    Revert "reproducer for SPR-750"
    This reverts commit f36f22abc7a16a9f6f69d19f705b9c364d27a245.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.1.1"
+	id("org.springframework.boot") version "3.1.2"
 	id("io.spring.dependency-management") version "1.1.0"
 	id("com.netflix.dgs.codegen") version "5.12.4"
 	kotlin("jvm") version "1.8.22"
